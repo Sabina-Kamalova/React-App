92d9416830e599d6d59486fc85eab97f4910d86c
Sabina Kamalova <skamalova09@gmail.com>
2023-03-11 17:55:47 +0100

    Data modification

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,23 +1,28 @@
-import { useState } from 'react';
-import styles from './CardForm.module.scss';
-import TextInput from '../TextInput/TextInput';
-import Button from '../Button/Button';
+import { useState } from "react";
+import styles from "./CardForm.module.scss";
+import TextInput from "../TextInput/TextInput";
+import Button from "../Button/Button";
+import { useDispatch } from "react-redux";
 
-const CardForm = props => {
+const CardForm = (props) => {
+  const [title, setTitle] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_CARD",
+      payload: { columnId: props.columnId, title },
+    });
+    setTitle("");
+  };
 
-const [title, setTitle] = useState('');
-    const handleSubmit = e => {
-        e.preventDefault()
-        props.action({title: title}, props.columnId);
-        setTitle('');
-    }
-
-    return (
-        <form onSubmit={handleSubmit} className={styles.cardForm}>
-            Title: <TextInput value={title} onChange={e => setTitle(e.target.value)} />
-            <Button>Add card</Button>
-        </form>
-    )
-}
+  return (
+    <form onSubmit={handleSubmit} className={styles.cardForm}>
+      Title:{" "}
+      <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      <Button>Add card</Button>
+    </form>
+  );
+};
 
 export default CardForm;
