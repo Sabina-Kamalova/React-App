7cab9c69b9c146cc5fe51df7a693adbadb35430a
Sabina Kamalova <skamalova09@gmail.com>
2023-03-19 21:10:22 +0100

    Add removeCard. Modify component Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,26 +2,30 @@ import styles from './Card.module.scss';
 import clxs from "clsx";
 import { useDispatch } from 'react-redux';
 import { toggleCardFavorite } from '../../redux/cardsRedux';
-import { useState } from 'react';
+//import { useState } from 'react';
+import { removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
 
-	const [isFavorite, setIsFavorite] = useState(props.isFavorite);
-
 	const dispatch = useDispatch()
 
-
-	const handleClick = (e) => {
+	const addToFavorite = (e) => {
 	e.preventDefault();
 	dispatch(toggleCardFavorite( props.cardId ));
-	 setIsFavorite(current => !current);
- 	console.log('isFavorite', isFavorite)
+	}
+
+	const deleteCard = (e) => {
+		e.preventDefault();
+		dispatch(removeCard( props.cardId ));
 	}
 
 		return (
   		<li className={styles.card}>
-				{props.title}
-				<button onClick={handleClick} className={clxs(styles.button)}><i className={clxs("fa fa-star-o", styles.icon, isFavorite && styles.isFavorite)}></i></button>
+			{props.title}
+			<div className={styles.container}>
+				<button onClick={addToFavorite} className={clxs(styles.button)}><i className={clxs("fa fa-star-o", styles.icon, props.isFavorite && styles.isFavorite)}></i></button>
+				<button onClick={deleteCard} className={clxs(styles.button)}><i className={clxs("fa fa-trash", styles.icon)}></i></button>
+			</div>
 		</li>
   	);
 };
